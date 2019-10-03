f7345358245ebfebae74977c1326140caeecba02
c_guhaibin-001 <guhaibin@ihandy.cn>
2019-10-03 17:48:46 +0800

    dao update

--- src/main/java/com/example/practise/dao/CarRelatedInfoJpaMapper.java
+++ src/main/java/com/example/practise/dao/CarRelatedInfoJpaMapper.java
@@ -12,5 +12,5 @@ import org.springframework.stereotype.Repository;
  */
 @Repository
 public interface CarRelatedInfoJpaMapper extends JpaRepository<CarRelatedInfoEntity,String> {
-//    Page<CarRelatedInfoEntity> findAll(int i, int i1);
+//    Page<CarRelatedInfoEntity> findAll(int i, int i1);alksjdf;lasjkf
 }
